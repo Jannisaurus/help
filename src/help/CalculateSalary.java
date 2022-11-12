5d601f15e26a27e71de817835f3fdf0ebde2835a
Jannisaurus <117642071+Jannisaurus@users.noreply.github.com>
2022-11-12 16:48:38 +0100

    bubbles
    and sparkles

---
+++ src/help/CalculateSalary.java
@@ -0,0 +1,45 @@
+package help;
+
+import java.util.Scanner; 
+
+public class CalculateSalary {
+	public static void main(String[] args) {
+		int sum = 1000; 
+		final double FLAT_TAX_RATE = 0.7;
+		PaymentSlip employee1 = new PaymentSlip();
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("What is the employee's name?");
+		String name = myScanner.nextLine();
+		
+		System.out.println("What is their hourly rate?");
+		int hourlyRate = myScanner.nextInt();
+		
+		System.out.println("How many hours did they work this month?");
+		int hours = myScanner.nextInt();
+		
+		//System.out.println(name + hourlyRate + hours);
+		
+		String tableBorders = "--------------------\n";
+		String tableHeaders = "## PAYMENT INFORMATION ##\r\n";
+		
+		String row1 = "Name:\t\t\t" + employee1.getName() + "\n";
+		String row2 = "Hourly rate:\t\tBiskopsgatan 5\n";
+		String row3 = "Hours worked:\t\tHelgonavägen 2\n";
+		String row4 = "Salary before tax:\tHelgonavägen 2\n";
+		String row5 = "Salary after tax:\tHelgonavägen 2\n";
+		
+		String rows = row1 + row2 + row3 + "\n" + row4 + row5;
+		
+		String output = tableBorders + tableHeaders + rows;
+		output += "\nIf you have any questions regarding your salary, contact Bob\n";
+		output += tableBorders; 
+		
+		System.out.println(output);
+		
+		//double test = sum * FLAT_TAX_RATE;
+		
+		//System.out.println(test);
+	}
+}
---
+++ src/help/PaymentSlip.java
@@ -0,0 +1,37 @@
+package help;
+
+public class PaymentSlip {
+	private String name; 
+	private int hourlyRate;
+	private int hours; 
+	
+	final double FLAT_TAX_RATE = 0.7;
+	
+	public PaymentSlip() {
+	}
+	
+	public PaymentSlip(String name, int hourlyRate, int hours) {
+		this.name = name;
+		this.hourlyRate = hourlyRate;
+		this.hours = hours;
+	}
+	public String getName() {
+		return name; 
+	}
+	public void setName() {
+		this.name = name; 
+	}
+	public int getHourlyRate() {
+		return hourlyRate; 
+	}
+	public void setHourlyRate() {
+		this.hourlyRate = hourlyRate; 
+	}
+	public int getHours() {
+		return hours; 
+	}
+	public void setHours() {
+		this.hours = hours; 
+	}
+
+}
